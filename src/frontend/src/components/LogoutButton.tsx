4048de005db998c8de7d3a638f69cb863d4de321
WildElaeagnus <54314123+akorzunin@users.noreply.github.com>
2022-06-28 02:13:52 +0300

    delete cookies on logout

---
+++ src/frontend/src/components/LogoutButton.tsx
@@ -0,0 +1,21 @@
+
+import React from 'react'
+import Button from './Button'
+import ClickButton from './ClickButton'
+import * as cookieHandle from './utils/cookieHandle'
+
+
+const LogoutButton = ({ButtonStyle}) => {
+    return (
+        <div onClick={ cookieHandle.deleteCookies }>
+            <Button
+                style={ButtonStyle}
+                title="Logout"
+                link="/"
+                color="bg-red-500"
+            />
+        </div>
+    )
+}
+
+export default LogoutButton
