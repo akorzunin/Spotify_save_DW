5189bba876f00315ac005871612bc9ca49317ee7
WildElaeagnus <54314123+akorzunin@users.noreply.github.com>
2022-08-08 12:35:22 +0300

    ಠ_ಠ

---
+++ src/frontend/src/components/utils/utils.ts
@@ -0,0 +1,20 @@
+const text_emojis = [
+    "┬─┬ ノ(゜ - ゜ノ)",
+    "(╯°□°）╯︵ ┻━┻",
+    "☜(ﾟヮﾟ☜)",
+    "( ´･･)ﾉ(._.`)",
+    "(｡･∀･)ﾉﾞ",
+    "(ToT)/~~~",
+    "(∪.∪ )...zzz",
+    "ヾ(^▽^*)))",
+    "(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧",
+    "✪ ω ✪",
+    "(☞ﾟヮﾟ)☞",
+    "☜(⌒▽⌒)☞",
+    "༼ つ ◕_◕ ༽つ",
+    "ಠ_ಠ",
+]
+
+export const get_text_emoji = (): String => {
+    return text_emojis[Math.floor(Math.random() * text_emojis.length)]
+}
