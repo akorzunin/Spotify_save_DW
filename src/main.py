b51c430259c60582a35bfeb7d33dba16eea0c32e
WildElaeagnus <54314123+akorzunin@users.noreply.github.com>
2022-09-02 23:11:24 +0300

    hide docs on prod

--- src/main.py
+++ src/main.py
@@ -1,14 +1,17 @@
-'''Main app for Spotify DW saver web server'''
+"""Main app for Spotify DW saver web server"""
+import os
 import asyncio
 import logging
 from starlette.routing import Mount
 from starlette.staticfiles import StaticFiles
-
 from fastapi import FastAPI
+from fastapi.openapi.docs import get_swagger_ui_html
+from fastapi.openapi.utils import get_openapi
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import JSONResponse
+
 from backend.app.task_handler import revive_user_tasks, task_tick
 from backend.metadata import tags_metadata
-
 from backend.app.api_routes import router as api_routes
 from backend.app.front_routes import router as front_routes
 from backend.app.logger import format as log_format
@@ -16,12 +19,34 @@ from backend.app.logger import format as log_format
 templates = Jinja2Templates(directory="src/frontend/templates")
 
 routes = [
-    Mount('/frontend/static', app=StaticFiles(directory='src/frontend/static',), name='static'),
+    Mount(
+        "/frontend/static",
+        app=StaticFiles(
+            directory="src/frontend/static",
+        ),
+        name="static",
+    ),
 ]
 app = FastAPI(
     openapi_tags=tags_metadata,
     routes=routes,
+    docs_url=None,
+    redoc_url=None,
+    openapi_url=None,
 )
+
+if DEBUG := bool(eval(os.getenv("DEBUG", "False"))):
+
+    @app.get("/docs")
+    async def get_documentation():
+        return get_swagger_ui_html(openapi_url="/openapi.json", title="docs")
+
+    @app.get("/openapi.json" )
+    async def get_open_api_endpoint():
+        return JSONResponse(
+            get_openapi(title="FastAPI", version=1, routes=app.routes)
+        )
+
 
 @app.on_event("startup")
 async def startup_event():
@@ -51,7 +76,7 @@ app.include_router(
     tags=["API"],
 )
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import uvicorn
     from configs.uvicorn import uvicorn_conf
 
