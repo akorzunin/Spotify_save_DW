95a016c43edbf96f3f8929bc6fdb93fc84fab842
WildElaeagnus <54314123+akorzunin@users.noreply.github.com>
2022-11-02 23:01:34 +0300

    fix ssl certs and change user to nonroot

--- src/configs/uvicorn.py
+++ src/configs/uvicorn.py
@@ -2,11 +2,14 @@ import uvicorn
 import os
 log_config = uvicorn.config.LOGGING_CONFIG
 log_config["formatters"]["access"]["fmt"] = "%(asctime)s - %(levelname)s - %(message)s"
+DEBUG = bool(eval(os.getenv('DEBUG', 'False')))
 uvicorn_conf = dict(
     app='main:app',
     host=os.getenv('HOST'),
     port=int(os.getenv('PORT')),
     log_level='info',
     log_config=log_config,
-    reload=bool(eval(os.getenv('DEBUG', 'False'))),
+    reload=DEBUG,
+    ssl_keyfile=None if DEBUG else './certbot/privkey.pem',
+    ssl_certfile=None if DEBUG else './certbot/fullchain.pem',
 )
