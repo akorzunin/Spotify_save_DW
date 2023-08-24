5a749e55945416a35fd24a0eeb526eef51e75ef3
akorzunin <akorzunin123@gmail.com>
2023-08-24 05:47:51 +0000

    fix(ui): remove overcomplicated code

--- src/frontend/src/components/WeekCounter.tsx
+++ src/frontend/src/components/WeekCounter.tsx
@@ -1,15 +1,10 @@
-// @ts-nocheck
-import React, { useState, useEffect, FC } from 'react';
+import React from 'react';
 import { weekNumber } from '../utils/timeMangment';
 
-export const WeekCounter: FC = () => {
-    const [currentWeek, setcurrentWeek] = useState('00');
-    useEffect(() => {
-        setcurrentWeek(weekNumber);
-    }, []);
+export const WeekCounter: React.FC = () => {
     return (
         <div className="text-white text-sm xl:text-lg p-0.5 text-shadow-md">
-            Current week: {currentWeek}
+            Current week: {weekNumber}
         </div>
     );
 };
