a2004f401da12051f631b14e6ae9303d6950a36b
akorzunin <akorzunin123@gmail.com>
2024-03-09 20:39:23 +0300

    fix: upd easter egg

--- src/frontend/src/components/EasterEgg.tsx
+++ src/frontend/src/components/EasterEgg.tsx
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { get_text_emoji, updateTextEmoji } from '../utils/utils';
 import { useAtom } from 'jotai';
 import { easterEggCountAtom } from '../store/store';
+import { cn } from '../lib/utils';
 
 export const EasterEgg = () => {
   const [easterEggCount, setEasterEggCount] = useAtom(easterEggCountAtom);
+  const [rotate, setRotate] = useState(false);
   return (
     <span
-      className="mt-2 w-full select-none  text-center text-sm text-gray-500 hover:cursor-help sm:ml-auto sm:mt-0 sm:w-auto sm:text-left"
+      className={cn(
+        'mt-2 w-full select-none  text-center text-sm text-gray-500 hover:cursor-help sm:ml-auto sm:mt-0 sm:w-auto sm:text-left',
+        rotate && 'animate-spin'
+      )}
       onClick={(e) => {
         setEasterEggCount(easterEggCount + 1);
-        console.log(easterEggCount);
+        if (easterEggCount > 10) {
+          setRotate(true);
+          setEasterEggCount(0);
+        } else {
+          setRotate(false);
+        }
+
         updateTextEmoji(e);
       }}
     >
