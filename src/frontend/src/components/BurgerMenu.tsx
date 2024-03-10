fdf584b615cf50f2e1c62e0a22effde469d08620
akorzunin <akorzunin123@gmail.com>
2024-03-10 12:40:10 +0300

    fix: burger menu styles

--- src/frontend/src/components/BurgerMenu.tsx
+++ src/frontend/src/components/BurgerMenu.tsx
@@ -1,20 +1,26 @@
 import React, { FC, useEffect, useState } from 'react';
 import Button from './buttons/BaseButton';
 import { deleteCookies } from '../utils/cookieHandle';
+import { cn } from '../lib/utils';
 
 interface IBurgerMenu {
   burgerClass: string;
 }
 
 const BurgerMenu: FC<IBurgerMenu> = ({ burgerClass }) => {
-  const [burgerOpen, setburgerOpen] = useState('');
+  const [burgerOpen, setburgerOpen] = useState(false);
   useEffect(() => {
-    setburgerOpen(burgerClass ? 'popup_open' : '');
+    setburgerOpen(!!burgerClass);
   }, [burgerClass]);
 
   return (
-    <div className={`popup ${burgerOpen} [clip-path:circle(70%_at_100%_30%)]`}>
-      <div className="my-auto ml-auto mr-7 flex flex-col">
+    <div
+      className={cn(
+        `ml-[calc((100vw - 320px) / 2);] fixed right-0 top-0 hidden h-80 w-48 bg-cyan-400 [clip-path:circle(70%_at_100%_30%)] lg:hidden`,
+        burgerOpen && 'flex'
+      )}
+    >
+      <div className="my-auto ml-auto mr-7 flex flex-col gap-3">
         <Button
           style="text-white"
           title="Help"
