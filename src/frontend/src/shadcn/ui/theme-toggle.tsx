c674cbd9a1eec35cebaaf1c1f1d2374cd95ceacf
akorzunin <akorzunin123@gmail.com>
2024-11-25 10:07:25 +0300

    chore: moved shadcn components

---
+++ src/frontend/src/shadcn/ui/theme-toggle.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Moon, Sun } from 'lucide-react';
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from './dropdown-menu';
+import { useTheme } from './theme-provider';
+import { Button } from './button';
+import { cn } from '../../lib/utils';
+
+export function ModeToggle({ className }: { className?: string }) {
+  const { setTheme } = useTheme();
+
+  return (
+    <div className={cn('contents', className)}>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="outline" size="icon">
+            <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+            <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+            <span className="sr-only">Toggle theme</span>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuItem onClick={() => setTheme('light')}>
+            Light
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setTheme('dark')}>
+            Dark
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setTheme('pepega-green')}>
+            Pepega green
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setTheme('system')}>
+            System
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+}
