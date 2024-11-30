5b968759807412b15c497bdf385d5dd3916f43a7
akorzunin <akorzunin123@gmail.com>
2024-12-01 01:17:15 +0300

    feat: temporary fixed blob button

--- src/frontend/src/components/buttons/blob-button/BlobButton.tsx
+++ src/frontend/src/components/buttons/blob-button/BlobButton.tsx
@@ -7,36 +7,35 @@ interface IBlobButton {
   link: string;
 }
 
-const circleStyle = {
+const circleStyle: Record<string, string> = {
   0: styles.circle__0,
   1: styles.circle__1,
   2: styles.circle__2,
   3: styles.circle__3,
   4: styles.circle__4,
 };
+const circles = Object.keys(circleStyle);
 
 const BlobButton: FC<IBlobButton> = ({ title = 'Blob', link = '/' }) => {
-  const circles = [0, 1, 2, 3, 4];
-
   return (
     <>
-      {/* <div className={`relative w-44 overflow-hidden ${styles.circle}`}> */}
-      {/* <Blob styles="img-responsive object-cover w-full h-full" /> */}
-      {/* {circles.map((circle) => {
+      <div className={`relative overflow-hidden ${styles.circle}`}>
+        {circles.map((circle) => {
           return (
             <div
               key={circle}
               className={`${styles.circle__main} ${circleStyle[circle]}`}
             ></div>
           );
-        })} */}
-      <a
-        href={link}
-        className="cursor-pointer rounded-full border border-transparent bg-opacity-0 text-xl font-bold text-purple-700 transition-opacity hover:opacity-80"
-      >
-        <button tabIndex={0}>{title}</button>
-      </a>
-      {/* </div> */}
+        })}
+        <a
+          href={link}
+          className="absolute flex h-48 w-48 cursor-pointer items-center justify-center rounded-full border border-transparent bg-opacity-0 text-xl font-bold text-primary-foreground transition-opacity hover:opacity-80"
+          tabIndex={0}
+        >
+          {title}
+        </a>
+      </div>
     </>
   );
 };
